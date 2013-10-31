4310c795d7460a0a6faac559ddbc3a2772bf7274
Andrew <andrewkim823@gmail.com>
2013-10-30 21:40:42 -0700

    Add collision detection
    Collision detection is added to the soldier script and CharacterController
    was removed from the soldiers.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+
+public class PlayerScript : MonoBehaviour {
+	public AudioClip boing;
+	public float walkSpeed = 10f;
+	public float rotationSpeed = 100.0f;
+	public float gravity = 10f;
+	
+	CharacterController controller;
+	Transform body;
+	GameObject camera;
+	Vector3 moveVector;
+	private Animation anim;
+	AudioSource audiosource;
+	CollisionFlags collisionFlags;
+	
+	// Use this for initialization
+	void Start () {
+		body = transform.FindChild("Body");
+		controller = GetComponent<CharacterController>();
+		if(body!=null){
+			body.renderer.material.color = Color.green;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(body!=null){
+			float inputRotate = Input.GetAxis("Horizontal");
+			moveVector = body.transform.forward*walkSpeed*Input.GetAxis("Vertical")*Time.deltaTime;
+			this.transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal")*rotationSpeed*Time.deltaTime,0));
+			collisionFlags = controller.Move(moveVector+new Vector3(0,-gravity*Time.deltaTime,0));
+		}
+	}
+}
---
+++ Assets/Scripts/SoldierScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoldierScript : MonoBehaviour {
+	
+	public float walkSpeed = 5f;
+	public float rotationSpeed = 100.0f;
+	public float gravity = 10f;
+	public int owner = 0;
+	private Animation anim;
+	Vector3 moveVector;
+	CollisionFlags collisionFlags;
+	GameObject king;
+	Transform body;
+	public float distance = 3;
+	
+	// Use this for initialization
+ 	void Start () {
+		king = GameObject.Find("Player");
+		body = transform.FindChild("Body");
+		if(body!=null){
+			body.renderer.material.color = Color.blue;
+		}
+		walkSpeed = Random.Range(50,100)/10f;
+		body.animation["Bounce"].speed = Random.Range(50,100)/100f;
+		body.animation.Play("Bounce");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(owner==0){
+			Follow (king);
+		}
+	}
+	
+	void Follow(GameObject target){
+		Quaternion direction = Quaternion.LookRotation(target.transform.position-transform.position);
+		direction.z = 0;
+		direction.x = 0;
+		transform.rotation = Quaternion.Slerp(transform.rotation,direction , rotationSpeed*Time.deltaTime);
+		if(Vector3.Distance(king.transform.position,transform.position)>distance){
+			transform.position = Vector3.MoveTowards(transform.position, target.transform.position, walkSpeed*Time.deltaTime);
+		}		
+	}
+	
+	void OnCollisionEnter(Collision c){
+		Debug.Log(c.gameObject.GetComponent<SoldierScript>().owner);
+	}
+}
